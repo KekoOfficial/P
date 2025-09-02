be47f38102ca0ebb87815a750d6a129ccbda8944
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2025-09-02 19:55:22 -0300

    Create logger.js

---
+++ utils/logger.js
@@ -0,0 +1,24 @@
+// utils/logger.js
+const fs = require('fs');
+
+const log = (message) => {
+    console.log(`> ✅ Log: ${message}`);
+};
+
+const logError = (message) => {
+    console.error(`> ❌ Error: ${message}`);
+};
+
+const appendLogFile = (filePath, content) => {
+    try {
+        fs.appendFileSync(filePath, content + '\n');
+    } catch (e) {
+        logError(`Error al escribir en el archivo de log: ${e.message}`);
+    }
+};
+
+module.exports = {
+    log,
+    logError,
+    appendLogFile,
+};
